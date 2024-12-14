0741bb170bb64bba1f995a7f7f10da25f8ddc64e
unknown <2002zainabaamir@gmail.com>
2024-12-14 00:34:49 -0500

    made a method for returns the product attributes

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ class ProductProperties {
         this.price = price;
         this.quantity = quantity;
     }
-
+    //method to return total value of product
     getTotalValue(){
         return ("Total Value: " + this.quantity*this.price);
     }
+
+    //method to state product attributes
+    toString(){
+        return (`Product: ${this.name} | Price: $${this.price} | Quantity: ${this.quantity}`);
+    }
+
 
 }
