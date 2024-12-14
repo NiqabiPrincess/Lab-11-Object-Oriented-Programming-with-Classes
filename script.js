064d33411612fdbcd94138ddf7520b11b2263daf
unknown <2002zainabaamir@gmail.com>
2024-12-14 00:45:49 -0500

    tested parent class, toString method successful

--- script.js
+++ script.js
@@ -19,10 +19,20 @@ class ProductProperties {
 //* child class
 
 class PerishableProductProperties extends ProductProperties {
+    //constructor adds another attribute
     constructor(name, price, quantity, expirationDate) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
         this.expirationDate = expirationDate;
     }
+
+    //method is overridden from parent class
+    toString(){
+        return (`Product: ${this.name} | Price: $${this.price} | Quantity: ${this.quantity}`);
+    }
 }
+
+//* testing parent class
+const lipstick = new ProductProperties ("Lipstick in Shade 'Rose Tea' ", 7.99, 20);
+console.log(lipstick.toString());
